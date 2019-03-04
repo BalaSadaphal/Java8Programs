0dd7dd0aaf226dfe3f32569f8336cc93a0bef1e9
HP <HP@LAPTOP-SMQMVM0Q>
2019-03-04 22:27:31 +0530

    --lambda

---
+++ Java8Programs/src/com/programs/lambda/LmbdaFunctionAsReturnTypeAndParameter.java
@@ -0,0 +1,25 @@
+package com.programs.lambda;
+
+interface ComplexMaths{
+	public int multiply(int x);
+}
+interface Maths{
+	public int calculate();
+}
+
+
+public class LmbdaFunctionAsReturnTypeAndParameter {
+	
+	static Maths addition(int a,int b,ComplexMaths calculation) {
+		return ()-> calculation.multiply(a)+ calculation.multiply(b);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(addition(10, 20, x-> x*x).calculate());
+		System.out.println(addition(2, 2, x-> x*x*x).calculate());
+	}
+	
+	
+	
+
+}
