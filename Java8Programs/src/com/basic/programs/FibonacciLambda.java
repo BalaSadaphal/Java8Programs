020cebc8305f967d65baa26677f3a73a79bd9ebc
HP <HP@LAPTOP-SMQMVM0Q>
2019-03-04 20:55:57 +0530

    --first commit

---
+++ Java8Programs/src/com/basic/programs/FibonacciLambda.java
@@ -0,0 +1,24 @@
+package com.basic.programs;
+
+//@FunctionalInterface
+interface FibonacciCalculator {
+	void fibonacci(int limit);
+}
+
+public class FibonacciLambda {
+	public static void main(String[] args) {
+		FibonacciCalculator calculate = (int limit) -> {
+			int previous = 0;
+			int current = 1;
+			System.out.println("Fibonacci Series :");
+			while (current < limit) {
+				int temp = current;
+				current = previous + current;
+				previous = temp;
+				System.out.print(current + " ");
+			}
+		};
+		calculate.fibonacci(20);
+	}
+
+}
---
+++ Java8Programs/src/com/basic/programs/FunctionalProgramming.java
@@ -0,0 +1,35 @@
+package com.basic.programs;
+
+interface Operations {
+	int operate(int a);
+}
+
+public class FunctionalProgramming {
+	static int sum(int a, int b, Operations operations) {
+		return operations.operate(a) + operations.operate(b);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Passing lambda as a parameter\n");
+
+		System.out.println("Sum of Sqaures of numbers 1, 2 ->    " + sum(1, 2, a -> a * a));
+		System.out.println("Sum of Cubes of numbers 1, 2   ->    " + sum(1, 2, a -> a * a * a));
+
+		System.out.println("\n<===========================>\n");
+
+		int nCr = nCr(10, 2, x -> {
+			int i = x - 1;
+			while (i > 0) {
+				x = x * i;
+				i--;
+			}
+			return x;
+		});
+		System.out.println("nCr = " + nCr);
+	}
+
+	static int nCr(int n, int r, Operations operations) {
+		return operations.operate(n) / (operations.operate(n - r) * operations.operate(r));
+	}
+
+}
