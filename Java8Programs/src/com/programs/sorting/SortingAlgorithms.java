8559655b0e2f6c457adb547348cac846774bb31c
HP <HP@LAPTOP-SMQMVM0Q>
2019-03-06 20:24:46 +0530

    --selection sort

--- Java8Programs/src/com/programs/sorting/SortingAlgorithms.java
+++ Java8Programs/src/com/programs/sorting/SortingAlgorithms.java
@@ -6,26 +6,29 @@ public class SortingAlgorithms {
 	static int counterForCountingComplexity;
 	public static void main(String[] args) {
 		int arr[]= {5 ,1 ,4 ,2 ,8};
-		bubbleSort(arr);
+		selectionSort(arr);
 		System.out.println("Final Result::");
 		System.out.println(Arrays.toString(arr));
 		System.out.println("Time complexity::"+counterForCountingComplexity);
 	}
 /*
-Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the 
+adjacent elements if they are in wrong order.
 Example:
 First Pass:
 ( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
 ( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
 ( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
-( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), 
+algorithm does not swap them.
 
 Second Pass:
 ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
 ( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
 ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
 ( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
-Now, the array is already sorted, but our algorithm does not know if it is completed. The algorithm needs one whole pass without any swap to know it is sorted.
+Now, the array is already sorted, but our algorithm does not know if it is completed. 
+The algorithm needs one whole pass without any swap to know it is sorted.
 
 Third Pass:
 ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
@@ -67,6 +70,8 @@ Stable: Yes
 		return arr;
 	}
 /*
+ * Works on index, identifies smaller elemt index and swaps that element with the i th element
+ * where i is outer loop pointer
  The selection sort algorithm sorts an array by repeatedly finding the minimum element 
  (considering ascending order) from unsorted part and putting it at the beginning. 
  The algorithm maintains two subarrays in a given array.
@@ -95,22 +100,27 @@ Following example explains the above steps:s
 // Find the minimum element in arr[3...4]
 // and place it at beginning of arr[3...4]
 11 12 22 25 64 	
+
+Time Complexity: 
+Best: O(n^2)
+Average: O(n^2)
+Worst: O(n^2)
  */
 	public static int[] selectionSort(int[] arr) {
 		counterForCountingComplexity=0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length - 1; j++) {
-				int current = arr[j];
-				int compareWith = arr[j + 1];
-				if (current > compareWith) {
-					int temp = arr[j];
-					arr[j] = compareWith;
-					arr[j + 1] = temp;
-				}
-				System.out.println("Loop No "+i);
-				System.out.println(Arrays.toString(arr));
-				counterForCountingComplexity++;
-			}
+		for (int i = 0; i < arr.length-1; i++) {
+			 int index = i;  
+	         for (int j = i + 1; j < arr.length; j++){  
+	        	 if (arr[j] < arr[index]){  
+	                    index = j;//searching for lowest index  
+	              }  
+	        	 counterForCountingComplexity++;
+	         }  
+	            int smallerNumber = arr[index];   
+	            arr[index] = arr[i];  
+	            arr[i] = smallerNumber;
+	            System.out.println("Loop No "+i);
+	            System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
