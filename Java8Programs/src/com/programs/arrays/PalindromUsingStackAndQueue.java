dd36ffdf50b7b19c85647fab7541279d471db928
HP <HP@LAPTOP-SMQMVM0Q>
2019-03-06 22:25:20 +0530

    --palindrome

--- Java8Programs/src/com/programs/arrays/PalindromUsingStackAndQueue.java
+++ Java8Programs/src/com/programs/arrays/PalindromUsingStackAndQueue.java
@@ -2,7 +2,6 @@ package com.programs.arrays;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class PalindromUsingStackAndQueue {
